f92ccd2d8fe306e764e56650142a7455931e2f52
Nicolas <ncorantehuenchuman@gmail.com>
2024-09-12 13:18:43 -0300

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import Juegos from './components/Juegos.vue';
+export default{
+  components:{
+    Juegos
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>Tienda 32 Bits</h1>
+  </div>
+  <div>
+    <Juegos/>
+  </div>
+  
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Juegos.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h3>Lista de Juegos</h3>
+    </div>
+</template>
